592f6e5deb7a03a76156a79d0219c6e2b8267db5
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-05-05 17:48:06 +0300

    Create an image of a given string
    Take images of separate letters
    (are present as items of `characters` list)
    and paste them together in order of letters in string

---
+++ draw_string.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from draw_letters import create_letters_images
+
+
+def draw_string(string, characters):
+    """
+    Creates an image of given string
+    :param string: string to be drawn
+    :param characters: images of letters that are presented in string
+    :return: image of string
+    """
+    images = [characters[c] for c in string]
+
+    width = sum(character.size[0] for character in images)
+    height = max(character.size[1] for character in images)
+
+    image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    horizontal_offset = 0
+    for character in images:
+        image.paste(character, (horizontal_offset, 0))
+        horizontal_offset += character.size[0]
+
+    return image
+
+
+if __name__ == "__main__":
+    letters = ['A', 'B', 'C']
+    size = 200
+    characters = create_letters_images(letters, size)
+    string = "ABCCAABBB"
+    image = draw_string(string, characters)
+    image.show()
