72f64d9435a095ffa34ecb38dd8907cab0bdae9a
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-05-05 17:14:55 +0300

    Create images of the given characters (letters) with the same sizes
    Use `PIL.ImageDraw.Draw.text` function to write a text (letters) in images.
    Tutorial: https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html

---
+++ draw_letters.py
@@ -0,0 +1,49 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def create_letters_images(characters, size):
+    """
+    Create images of characters (letters)
+    :param characters: a list of letters that should be presented as images
+    :param size: size of characters
+    :return: generated images of given characters
+    """
+    font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf', size)
+    images = {}
+    for c in characters:
+        width, height = font.getsize(c)
+        c_width, c_height = font.getoffset(c)
+        image = Image.new('RGBA', (width, height), color=(255, 255, 255, 0))
+        draw = ImageDraw.Draw(image)
+        draw.text((-c_width, -c_height), text=c, fill='black', font=font)
+
+        # Crop image (remove areas with transparent background)
+        bbox = image.convert("RGBa").getbbox()
+        images[c] = image.crop(bbox)
+
+    images = align_image_sizes(characters, images)
+    return images
+
+
+def align_image_sizes(characters, images):
+    """
+    Resize images so that they have the same sizes
+    :param characters: a list of letters
+    :param images: generated images with different sizes
+    :return: resized images
+    """
+    width = max(images[c].size[0] for c in characters)
+    height = max(images[c].size[1] for c in characters)
+    for c in characters:
+        image = images[c]
+        images[c] = image.resize((width, height), Image.ANTIALIAS)
+
+    return images
+
+
+if __name__ == "__main__":
+    characters = ['A', 'B', 'C']
+    size = 200
+    images = create_letters_images(characters, size)
+    for c in characters:
+        images[c].show()
