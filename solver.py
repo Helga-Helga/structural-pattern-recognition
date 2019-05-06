50f879360c50e6810402a04c48c8623644ce88fe
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-05-06 19:06:43 +0300

    Implement solver based on dynamic programming

--- solver.py
+++ solver.py
@@ -22,6 +22,30 @@ def min_solver(nodes):
     return argmin_nodes
 
 
+def dynamic_programming_solver(nodes, edges):
+    """
+    Find labeling using dynamic programming algorithm
+    :param nodes: 2d array of node weights
+    :param edges: 4d array of edge weights
+    :return: array of letter indexes
+    """
+    argmin_nodes = zeros(nodes.shape[0], dtype=int)
+    for i in range(nodes.shape[0]):
+        for k in range(nodes.shape[1]):
+            min_value = inf
+            for k_ in range(nodes.shape[1]):
+                if i == nodes.shape[0] - 1:
+                    if nodes[i, k_] < min_value:
+                        min_value = nodes[i, k_]
+                        argmin_nodes[i] = k_
+                    continue
+                if edges[i, i+1, k_, k] + nodes[i, k_] < min_value:
+                    min_value = edges[i, i+1, k_, k] + nodes[i, k_]
+                    argmin_nodes[i] = k_
+            nodes[i, k] += min_value
+    return argmin_nodes
+
+
 def get_result_string(argmin_nodes, labels):
     """
     Get string for found labeling
@@ -36,15 +60,17 @@ def get_result_string(argmin_nodes, labels):
 
 
 if __name__ == "__main__":
-    n = 12
+    n = 20
     labels = ['A', 'B', 'C', ' ']
     string = generate_string(labels, n)
     characters = create_letters_images(labels, 200)
     image = draw_string(string, characters)
-    print("True string: {}".format(string))
+    print("Input string:           {}".format(string))
 
     nodes, edges = initialize_graph(n, labels)
     list_characters = [c for c in characters.values()]
     image = image.convert("L")
     nodes = fill_nodes(nodes, image, list_characters)
-    print("Recognized:  {}".format(get_result_string(min_solver(nodes), labels)))
+    print("Recognized taking min:  {}".format(get_result_string(min_solver(nodes), labels)))
+
+    print("Recognized dynamically: {}".format(get_result_string(dynamic_programming_solver(nodes, edges), labels)))
